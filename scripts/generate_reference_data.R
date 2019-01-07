9222c32d90a1f76f8feb712faa68542f09980306
Robert Ashton <r.ashton@imperial.ac.uk>
2019-01-07 10:28:19 +0000

    Expand test coverage

--- scripts/generate_reference_data.R
+++ scripts/generate_reference_data.R
@@ -3,13 +3,33 @@ devtools::load_all()
 test_cases = c("Botswana2017", "Botswana2018", "Mozambique_Maputo_Cidade2018")
 for (test_case in test_cases) {
   print(test_case)
-  pjnz <- system.file("extdata/testpjnz",
-                      paste(test_case, ".PJNZ", sep = ""),
-                      package = "epp")
-  epp_data <- read_epp_data(pjnz)
   dir.create("tests/testthat/reference", FALSE, TRUE)
-  saveRDS(epp_data, paste("tests/testthat/reference/", 
+  pjnz_path <- system.file("extdata/testpjnz",
+                           paste(test_case, ".PJNZ", sep = ""),
+                           package = "epp")
+  
+  hiv_prevalence <- read_epp_data(pjnz_path)
+  saveRDS(hiv_prevalence, paste("tests/testthat/reference/", 
+                                test_case,  
+                                "_hiv_prevalence_data.rds", 
+                                sep = ""))
+  
+  
+  population_data <- read_epp_subpops(pjnz_path)
+  saveRDS(population_data, paste("tests/testthat/reference/", 
+                                 test_case,
+                                 "_population_data.rds",
+                                 sep = ""))
+  
+  spt_data <- read_spt(pjnz_path)
+  saveRDS(spt_data, paste("tests/testthat/reference/", 
                           test_case,
-                          ".rds",
+                          "_spt_data.rds",
                           sep = ""))
+  
+  # spu_data <- read_spu(pjnz_path)
+  # saveRDS(spu_data, paste("tests/testthat/reference/", 
+  #                         test_case,
+  #                         "_spu_data.rds",
+  #                         sep = ""))
 }
--- tests/testthat/test_reference.R
+++ tests/testthat/test_reference.R
@@ -1,23 +1,52 @@
 context("reference")
 
-test_that("Botswana2017 epp data is read correctly", {
+test_that("Botswana2017 EPP data is read correctly", {
   pjnz <- system.file("extdata/testpjnz", "Botswana2017.PJNZ", package="epp")
-  epp_data <- epp::read_epp_data(pjnz)
-  ref <- readRDS("reference/Botswana2017.rds")
-  expect_equal(epp_data, ref)
+  
+  hiv_prevalence <- epp::read_epp_data(pjnz)
+  hiv_ref <- readRDS("reference/Botswana2017_hiv_prevalence_data.rds")
+  expect_equal(hiv_prevalence, hiv_ref)
+  
+  population_data <- epp::read_epp_subpops(pjnz)
+  population_ref <- readRDS("reference/Botswana2017_population_data.rds")
+  expect_equal(population_data, population_ref)
+  
+  spt_data <- epp::read_spt(pjnz)
+  spt_ref <- readRDS("reference/Botswana2017_spt_data.rds")
+  expect_equal(spt_data, spt_ref)
 })
 
-test_that("Botswana2018 epp data is read correctly", {
+test_that("Botswana2018 EPP data is read correctly", {
   pjnz <- system.file("extdata/testpjnz", "Botswana2018.PJNZ", package="epp")
-  epp_data <- epp::read_epp_data(pjnz)
-  ref <- readRDS("reference/Botswana2018.rds")
-  expect_equal(epp_data, ref)
+  
+  hiv_prevalence <- epp::read_epp_data(pjnz)
+  hiv_ref <- readRDS("reference/Botswana2018_hiv_prevalence_data.rds")
+  expect_equal(hiv_prevalence, hiv_ref)
+  
+  population_data <- epp::read_epp_subpops(pjnz)
+  population_ref <- readRDS("reference/Botswana2018_population_data.rds")
+  expect_equal(population_data, population_ref)
+  
+  spt_data <- epp::read_spt(pjnz)
+  spt_ref <- readRDS("reference/Botswana2018_spt_data.rds")
+  expect_equal(spt_data, spt_ref)
 })
 
-test_that("Mozambique_Maputo_Cidade2018 epp data is read correctly", {
+test_that("Mozambique_Maputo_Cidade2018 EPP data is read correctly", {
   pjnz <- system.file("extdata/testpjnz", "Mozambique_Maputo_Cidade2018.PJNZ", 
                       package="epp")
-  epp_data <- epp::read_epp_data(pjnz)
-  ref <- readRDS("reference/Mozambique_Maputo_Cidade2018.rds")
-  expect_equal(epp_data, ref)
+  
+  hiv_prevalence <- epp::read_epp_data(pjnz)
+  hiv_ref <- readRDS(
+    "reference/Mozambique_Maputo_Cidade2018_hiv_prevalence_data.rds")
+  expect_equal(hiv_prevalence, hiv_ref)
+  
+  population_data <- epp::read_epp_subpops(pjnz)
+  population_ref <- readRDS(
+    "reference/Mozambique_Maputo_Cidade2018_population_data.rds")
+  expect_equal(population_data, population_ref)
+  
+  spt_data <- epp::read_spt(pjnz)
+  spt_ref <- readRDS("reference/Mozambique_Maputo_Cidade2018_spt_data.rds")
+  expect_equal(spt_data, spt_ref)
 })
