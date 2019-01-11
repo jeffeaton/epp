d125140098bffa8e3ad5b072b5343dedfe30df46
Robert Ashton <r.ashton@imperial.ac.uk>
2019-01-11 11:23:12 +0000

    Add test case for Dominican Republic and improve naming

--- scripts/generate_reference_data.R
+++ scripts/generate_reference_data.R
@@ -1,6 +1,7 @@
 devtools::load_all()
 
-test_cases = c("Botswana2017", "Botswana2018", "Mozambique_Maputo_Cidade2018")
+test_cases = c("Botswana2017", "Botswana2018", "Mozambique_Maputo_Cidade2018",
+               "DominicanRepublic2017")
 for (test_case in test_cases) {
   print(test_case)
   dir.create("tests/testthat/reference", FALSE, TRUE)
@@ -8,23 +9,23 @@ for (test_case in test_cases) {
                            paste(test_case, ".PJNZ", sep = ""),
                            package = "epp")
   
-  hiv_prevalence <- read_epp_data(pjnz_path)
-  saveRDS(hiv_prevalence, paste("tests/testthat/reference/", 
+  epp_data <- read_epp_data(pjnz_path)
+  saveRDS(epp_data, paste("tests/testthat/reference/", 
                                 test_case,  
-                                "_hiv_prevalence_data.rds", 
+                                "_epp_data.rds", 
                                 sep = ""))
   
   
-  population_data <- read_epp_subpops(pjnz_path)
-  saveRDS(population_data, paste("tests/testthat/reference/", 
+  epp_subpop_data <- read_epp_subpops(pjnz_path)
+  saveRDS(epp_subpop_data, paste("tests/testthat/reference/", 
                                  test_case,
-                                 "_population_data.rds",
+                                 "_epp_subpop_data.rds",
                                  sep = ""))
   
-  incidence_prevalence_bf <- read_spt(pjnz_path)
-  saveRDS(incidence_prevalence_bf, paste("tests/testthat/reference/", 
+  spt_data <- read_spt(pjnz_path)
+  saveRDS(spt_data, paste("tests/testthat/reference/", 
                                    test_case,
-                                   "_incidence_prevalence_bf_data.rds",
+                                   "_spt_data.rds",
                                    sep = ""))
   
   # spu_data <- read_spu(pjnz_path)
